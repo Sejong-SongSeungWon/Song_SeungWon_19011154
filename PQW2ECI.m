0dfcca44102933fad75effee58757a85704336c6
82103 <82103@DESKTOP-FUEK8P7>
2023-05-21 22:15:50 +0900

    cosd,sind -> cos2d,sin2d

--- PQW2ECI.m
+++ PQW2ECI.m
@@ -6,9 +6,9 @@ function rotation_matrix = PQW2ECI(arg_prg, inc_angle, RAAN)
 % RAAN = right scension of the ascending node (capital omega)
 %% R/xyz = (R(arg_prg,3)*R(inc_angle,1)*R(RAAN,3))'*R/pqw
 
-R_arg_prg_3 = [cosd(arg_prg) sind(arg_prg) 0;-sind(arg_prg) cosd(arg_prg) 0;0 0 1];
-R_inc_angle_1 = [1 0 0;0 cosd(inc_angle) sind(inc_angle);0 -sind(inc_angle) cosd(inc_angle)];
-R_RAAN_3 = [cosd(RAAN) sind(RAAN) 0;-sind(RAAN) cosd(RAAN) 0;0 0 1];
+R_arg_prg_3 = [cos2d(arg_prg) sin2d(arg_prg) 0;-sin2d(arg_prg) cos2d(arg_prg) 0;0 0 1];
+R_inc_angle_1 = [1 0 0;0 cos2d(inc_angle) sin2d(inc_angle);0 -sin2d(inc_angle) cos2d(inc_angle)];
+R_RAAN_3 = [cos2d(RAAN) sin2d(RAAN) 0;-sin2d(RAAN) cos2d(RAAN) 0;0 0 1];
 
 rotation_matrix = ((R_arg_prg_3)*(R_inc_angle_1)*(R_RAAN_3))';
 
