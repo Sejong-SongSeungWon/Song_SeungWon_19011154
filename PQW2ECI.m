33c2e45f6f9791d28349bb6aca91983210b6f0da
82103 <82103@DESKTOP-FUEK8P7>
2023-05-21 21:53:13 +0900

    Code modification: line 10 & line 13

--- PQW2ECI.m
+++ PQW2ECI.m
@@ -1,16 +1,16 @@
 function rotation_matrix = PQW2ECI(arg_prg, inc_angle, RAAN)
 
-%% This function makes Perifocal frame to ECI frame.
+%% This function makes the Perifocal frame an ECI frame.
 % arg_prg = argument of perigee (small omega)
 % inc_angle = inclination angle (i)
 % RAAN = right scension of the ascending node (capital omega)
 %% R/xyz = (R(arg_prg,3)*R(inc_angle,1)*R(RAAN,3))'*R/pqw
 
 R_arg_prg_3 = [cosd(arg_prg) sind(arg_prg) 0;-sind(arg_prg) cosd(arg_prg) 0;0 0 1];
-R_inc_angle_1 = [1 0 0;0 cosd(inc_angle) sind(inc_angle);0 -sind(inc_angle) cosd(inc_angle);0 -sind(inc_angle) cos(inc_angle)];
+R_inc_angle_1 = [1 0 0;0 cosd(inc_angle) sind(inc_angle);0 -sind(inc_angle) cosd(inc_angle)];
 R_RAAN_3 = [cosd(RAAN) sind(RAAN) 0;-sind(RAAN) cosd(RAAN) 0;0 0 1];
 
-rotation_matrix = ((R_arg_prg_3)*(R_inc_angle_1)*(R_RAAN_3));
+rotation_matrix = ((R_arg_prg_3)*(R_inc_angle_1)*(R_RAAN_3))';
 
 % For example) R/xyz = PQW2ECI(30, 15, 30) * R/pqw
-% For Degree dimension
+% ** For Degree dimension **
