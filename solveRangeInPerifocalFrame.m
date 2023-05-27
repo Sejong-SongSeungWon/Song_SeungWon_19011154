41914e0f1d8f416689ee0b9c9f92deebc52b4884
82103 <82103@DESKTOP-FUEK8P7>
2023-05-27 17:09:22 +0900

    first_completion

--- solveRangeInPerifocalFrame.m
+++ solveRangeInPerifocalFrame.m
@@ -1,3 +1,13 @@
 function rangeInPQW = solveRangeInPerifocalFrame(semimajor_axis, eccentricity, true_anomaly)
 % unit : deg, km
 
+% ellipse elements
+a=semimajor_axis;
+e=eccentricity;
+c=a*e;
+b=sqrt(a^2-c^2);
+
+P=b*sqrt(1-(c^2)/(a^2));
+r_scalar=P/(1+e*cosd(true_anomaly));
+
+rangeInPQW = [r_scalar*cosd(ture_anomaly);r_scalar*sind(true_anomaly);0];
--- solveVelocityInPerifocalFrame.m
+++ solveVelocityInPerifocalFrame.m
@@ -1,2 +1,14 @@
 function velocityInPQW = solveVelocityInPerifocalFrame(semimajor_axis, eccentricity, true_anomaly)
 % unit : deg, km
+
+% ellipse elements
+a=semimajor_axis;
+e=eccentricity;
+c=a*e;
+b=sqrt(a^2-c^2);
+
+P=b*sqrt(1-(c^2)/(a^2));
+mu=3.986004418*10^14;
+r_scalar=P/(1+e*cosd(true_anomaly));
+
+velocityInPQW = sqrt(mu/P)*[-sind(true_anomaly);e+cosd(true_anomaly);0];
