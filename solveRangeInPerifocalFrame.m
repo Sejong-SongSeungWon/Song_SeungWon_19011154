f26e67bcbfc2e6a90e74ae8fcb2a1197d2859217
82103 <82103@DESKTOP-FUEK8P7>
2023-05-28 13:37:19 +0900

    Second Completion ( function P is modified )

--- solveRangeInPerifocalFrame.m
+++ solveRangeInPerifocalFrame.m
@@ -4,10 +4,8 @@ function rangeInPQW = solveRangeInPerifocalFrame(semimajor_axis, eccentricity, t
 % ellipse elements
 a=semimajor_axis;
 e=eccentricity;
-c=a*e;
-b=sqrt(a^2-c^2);
 
-P=b*sqrt(1-(c^2)/(a^2));
+P=a*(1-e^2);
 r_scalar=P/(1+e*cosd(true_anomaly));
 
 rangeInPQW = [r_scalar*cosd(ture_anomaly);r_scalar*sind(true_anomaly);0];
