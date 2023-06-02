136e9b86094daf72c937d836ff2218976d9fd0b1
82103 <82103@DESKTOP-FUEK8P7>
2023-06-02 14:15:49 +0900

    modification1

--- azimuth.m
+++ azimuth.m
@@ -1,15 +1,15 @@
 function az = azimuth(ENU)
 
-% Number of Column
-i=length(ENU);
+% Number of row = 위성수
+[numCols,numRows]=size(ENU);
 
 % Numbering each ENU elements of each satellites
-for k=0:i-1
+for k=0:numRows-1
     R_E(k+1)=ENU(3*k+1);
     R_N(k+1)=ENU(3*k+2);
 end
 
 % creating az matrix
-for j=0:i-1
+for j=0:numRows-1
     az(j+1)=cos2d(R_N(j+1),sqrt(R_E(j+1)^2+R_N(j+1)^2));
 end
