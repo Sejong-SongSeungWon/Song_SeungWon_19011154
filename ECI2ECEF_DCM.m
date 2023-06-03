dd219ee0830b4e3fad8115025fe8df2dd3da5ed8
82103 <82103@DESKTOP-FUEK8P7>
2023-06-03 22:32:10 +0900

    modification1

--- ECI2ECEF_DCM.m
+++ ECI2ECEF_DCM.m
@@ -1,2 +1,13 @@
 function DCM=ECI2ECEF_DCM(time)
 
+t0=juliandate([2000 1 1 12 0 0]);
+t=juliandate(time);
+
+THETAg_0 = siderealTime(t0);
+THETAg_t = siderealTime(t);
+
+THETAg = THETAg_0 + THETAg_t;
+
+DCM=[cosd(THETAg)  sind(THETAg) 0;
+     -sind(THETAg) cosd(THETAg) 0;
+     0             0            1 ];
