b896124bb7e9cb58ddc5b5de47f090c7d27416ea
82103 <82103@DESKTOP-FUEK8P7>
2023-06-03 22:58:03 +0900

    modification2

--- ECI2ECEF_DCM.m
+++ ECI2ECEF_DCM.m
@@ -1,13 +1,17 @@
 function DCM=ECI2ECEF_DCM(time)
 
+% Scalrize row vector as Juliandate
 t0=juliandate([2000 1 1 12 0 0]);
 t=juliandate(time);
 
+% Get GMST
 THETAg_0 = siderealTime(t0);
 THETAg_t = siderealTime(t);
 
+% Get Theta_g
 THETAg = THETAg_0 + THETAg_t;
 
+% Make DCM
 DCM=[cosd(THETAg)  sind(THETAg) 0;
      -sind(THETAg) cosd(THETAg) 0;
      0             0            1 ];
