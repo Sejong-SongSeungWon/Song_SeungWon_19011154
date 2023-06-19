68c09549376e090eec8d642320659512be7f3f09
82103 <82103@DESKTOP-FUEK8P7>
2023-06-19 13:32:15 +0900

    modi

--- azimuth.m
+++ azimuth.m
@@ -1,15 +1,24 @@
 function az = azimuth(ENU)
-
+%% 
 % Number of row = 위성수
-[numCols,numRows]=size(ENU);
+%[numCols,numRows]=size(ENU);
 
 % Numbering each ENU elements of each satellites
-for k=0:numRows-1
-    R_E(k+1)=ENU(3*k+1);
-    R_N(k+1)=ENU(3*k+2);
-end
+% for k=0:numRows-1
+%     R_E(k+1)=ENU(3*k+1);
+%     R_N(k+1)=ENU(3*k+2);
+% end
 
 % creating az matrix
-for j=0:numRows-1
-    az(j+1)=acosd(R_N(j+1)/sqrt(R_E(j+1)^2+R_N(j+1)^2));
+% for j=0:numRows-1
+%     az(j+1)=acosd(R_N(j+1)/sqrt(R_E(j+1)^2+R_N(j+1)^2));
+% end
+%% 
+
+r2d=180/pi;
+az=pi/2-atan2(ENU(1,:),ENU(2,:));
+az=az*r2d;
+%변형(삭제 필요)
+if az<0
+    az=180-az;
 end
