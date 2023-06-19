cd491fb9e3819ae9f5a6186df1272ce95154df64
82103 <82103@DESKTOP-FUEK8P7>
2023-06-19 13:35:38 +0900

    mo

--- solveRangeInPerifocalFrame.m
+++ solveRangeInPerifocalFrame.m
@@ -6,6 +6,6 @@ a=semimajor_axis;
 e=eccentricity;
 
 P=a*(1-e^2);
-r_scalar=P/(1+e*cosd(true_anomaly));
+r_scalar=P/(1+e*cos(true_anomaly));
 
-rangeInPQW = [r_scalar*cosd(ture_anomaly);r_scalar*sind(true_anomaly);0];
+rangeInPQW = [r_scalar*cos(true_anomaly);r_scalar*sin(true_anomaly);0];
