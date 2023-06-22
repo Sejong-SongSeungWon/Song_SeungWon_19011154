60b6e80d4bdfe6a484908807e43e5b655ccad9e6
82103 <82103@DESKTOP-FUEK8P7>
2023-06-22 14:20:50 +0900

    corr

--- ECI2ECEF_DCM.m
+++ ECI2ECEF_DCM.m
@@ -1,6 +1,6 @@
 function DCM=ECI2ECEF_DCM(time)
 
-theta_g=siderealTime(juliandatetime(time));
+theta_g=siderealTime(juliandate(time));
 DCM=[cosd(theta_g) sind(theta_g) 0;
     -sind(theta_g) cosd(theta_g) 0;
     0 0 1];
