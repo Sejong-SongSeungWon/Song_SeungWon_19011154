3067d0bc15ac749e24508f33024753000d6a463c
82103 <82103@DESKTOP-FUEK8P7>
2023-06-22 16:17:37 +0900

    correction

--- azimuth.m
+++ azimuth.m
@@ -1,19 +1,16 @@
 function az = azimuth(ENU)
  
-% Number of row = 위성수
-number=length(ENU);
-az=[];
 % Numbering each ENU elements of each satellites
-for k=1:number
-    R_E=ENU(k,1);
-    R_N=ENU(k,2);
+for k=1:length(ENU)
+    R_E=ENU(1,k);
+    R_N=ENU(2,k);
     R_scalar=sqrt(R_E^2+R_N^2);
     Ra=acosd(R_N/R_scalar);
 
     % creating az matrix
     if R_E >0
-        az=[az,Ra];
+        az(k)=Ra;
     else
-        az=[az,360-Ra];
+        az(k)=360-Ra;
     end
 end
--- elevation.m
+++ elevation.m
@@ -1,19 +1,15 @@
 function el = elevation(ENU, el_mask)
 
-% Number of row = 위성수
-    number=size(ENU);
-    el=[];
-
 % Numbering ENU elements of each satellites
-    for k=1:number
-        R_scalar=sqrt(ENU(k,1)^2+ENU(k,2)^2+ENU(k,3)^2);
-        R_U=ENU(k,3);
-        el_temp=asind(R_U/R_scalar);
+    for k=1:length(ENU)
+        R_scalar=sqrt(ENU(1,k)^2+ENU(2,k)^2+ENU(3,k)^2);
+        R_U=ENU(3,k);
+        el_a=asind(R_U/R_scalar);
 
-        if el_temp >= el_mask
-            el=[el,el_temp];
+        if el_a >= el_mask
+            el(k)=el_a;
         else
-            el=[el,NaN];
+            el(k)=NaN;
         end
     end
 end
